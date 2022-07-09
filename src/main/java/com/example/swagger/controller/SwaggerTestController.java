b4b44b2046b439beeac548dbecb9c2575c83804f
고의동 <kouydong@aegisep.com>
2022-07-10 06:30:29 +0900

    어노테이션 추가

--- src/main/java/com/example/swagger/controller/SwaggerTestController.java
+++ src/main/java/com/example/swagger/controller/SwaggerTestController.java
@@ -3,15 +3,23 @@ package com.example.swagger.controller;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.swagger.dto.req.SwaggerReqDto;
 import springfox.documentation.annotations.ApiIgnore;
 
+
+
 @RestController
 public class SwaggerTestController {
-
     @PostMapping("/swaggerTest")
     //─────────────────────────────────────────────────────────────────────────
     //  기능 : Controller안의 파라미터 설명시 사용 => @ApiImplicitParam 인터페이스 어노테이션 구현 필요
@@ -23,26 +31,32 @@ public class SwaggerTestController {
                     //─────────────────────────────────────────────────────────────────────────
                     //  속성값
                     //─────────────────────────────────────────────────────────────────────────
-                    //  name : 파라미터 이름 설정
+                    //  name  : 파라미터 이름 설정
                     //  value : 파라미터의 값 설정
                     //─────────────────────────────────────────────────────────────────────────
                     @ApiImplicitParam(name = "aptCd" , value = "아파트코드5자리"),
                     @ApiImplicitParam(name = "userId", value = "암호화유저아이디")
-            }
-            )
+            })
     //─────────────────────────────────────────────────────────────────────────
     //  기능 : Controller안의 메서드 설명시 사용
     //─────────────────────────────────────────────────────────────────────────
     //  속성값
     //─────────────────────────────────────────────────────────────────────────
-    //  value : 메소드가 무슨 기능을 하는지 요약 정의
-    //  notes : 메소드가 무슨 기능을 하는지에 대한 상세 정의
+    //  summary     : 메소드가 무슨 기능을 하는지 요약 정의
+    //  description : 메소드가 무슨 기능을 하는지에 대한 상세 정의
     //─────────────────────────────────────────────────────────────────────────
-    @ApiOperation(value = "스웨거테스트", notes = "스웨거 명세화를 위한 API 테스트")
+    @Operation(summary = "스웨거테스트", description ="사용자 정보 조회")
     //─────────────────────────────────────────────────────────────────────────
     //  기능 : Swagger API 메서드의 출력되지 않도록 설정
     //─────────────────────────────────────────────────────────────────────────
     //@ApiIgnore
+
+    //─────────────────────────────────────────────────────────────────────────
+    //  기능 : 그룹핑하는 어노테이션으로 name 속성이 같은 것끼리 API그룹으로 묶음
+    //  지정영역 : Class or Method
+    //─────────────────────────────────────────────────────────────────────────
+    //@Tag(name="MyController", description = "사용자 API")
+
     public String getTestApi(@RequestBody SwaggerReqDto reqDto) {
 
         String aptCd    = reqDto.getAptCd();
--- src/main/java/com/example/swagger/dto/req/SwaggerReqDto.java
+++ src/main/java/com/example/swagger/dto/req/SwaggerReqDto.java
@@ -1,11 +1,19 @@
 package com.example.swagger.dto.req;
 
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+import org.intellij.lang.annotations.Pattern;
+import org.intellij.lang.annotations.RegExp;
 
 @Setter
 @Getter
+//─────────────────────────────────────────────────────────────────────────────────────────────
+//  기능 : Swqgger 어노테이션으로 Request에 어떤 값들이 들어가는지 설정하기 위한 메타 어노테이션 (3.0에서는 작동하지 않음)
+//  영역 : 클래스 및 매서드
+//─────────────────────────────────────────────────────────────────────────────────────────────
+//@Schema(description = "아파트아이모듈 RequestBody")
 public class SwaggerReqDto {
 
     @ApiModelProperty(example="12345")
